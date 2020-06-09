7dcadeeb63b1ea9bebd59aa297c4c2b845df6c81
RFIDServer <1018925913@qq.com>
2020-06-09 22:48:22 +0800

    LH

--- src/Dao/daoservice.java
+++ src/Dao/daoservice.java
@@ -8,7 +8,7 @@ public class daoservice {
     private dao dao=new dao();
 
 
-    public void insert(parcel newParcel) throws SQLException, ClassNotFoundException {
+    public int insert(parcel newParcel) throws SQLException, ClassNotFoundException {
         dao.getConn();
         Object[] obj={newParcel.get物品类型(),newParcel.get现在地址(),newParcel.get发货人(),
                 newParcel.get收货人(), newParcel.get发货地址(),newParcel.get收货地址(),
@@ -17,9 +17,33 @@ public class daoservice {
                 "insert into parcels(物品类型,现在地址,发货人,收货人,发货地址,收货地址,发货人联系方式,收货人联系方式,下一站)" +
                 " values(?,?,?,?,?,?,?,?,?)" +
                 "",obj);
-        dao.update();
+        int i=dao.update();
 
         dao.closePtmt();
         dao.closeConn();
+        return i;
+    }
+
+    public int updata(String 现在地址,String 下一站,String cardID) throws SQLException, ClassNotFoundException {
+        dao.getConn();
+        Object[] obj={现在地址,下一站,cardID};
+        dao.createPtmt("update parcels set 现在地址=?,下一站=?  where cardID = ?",obj);
+        int i=dao.update();
+
+        dao.closePtmt();
+        dao.closeConn();
+        return i;
+    }
+
+    public int delete(String cardID) throws SQLException, ClassNotFoundException {
+        dao.getConn();
+        Object[] obj={cardID};
+        //DELETE FROM `rfid_test`.`parcels` WHERE (`id` = '2');
+        dao.createPtmt("delete from parcels where id=?",obj);
+        int i=dao.update();
+
+        dao.closePtmt();
+        dao.closeConn();
+        return i;
     }
 }
--- src/console/Service/daoTest.java
+++ src/console/Service/daoTest.java
@@ -9,10 +9,21 @@ public class daoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         daoservice daoservice=new daoservice();
 
-        parcel parcel=new parcel(null, "111", "111", "111",
-                "111", "111", "111", "111",
-                "111","111");
-        daoservice.insert(parcel);
-        System.out.println("success");
+        //插入测试
+//        parcel parcel=new parcel(null, "111", "111", "111",
+//                "111", "111", "111", "111",
+//                "111","111");
+//        if (daoservice.insert(parcel)==1){
+//            System.out.println("success");
+//        }else {
+//            System.out.println("faild");
+//        }
+
+        //更新数据
+//        System.out.println(daoservice.updata("cy","xx","1"));
+
+        //删除操作
+        System.out.println(daoservice.delete("2"));
+
     }
 }
